af1339ef40e04496f8711621741bce0eaccaf039
Josef Bischof <josefmb17@gmail.com>
2023-02-14 21:30:54 -0300

    iniciando components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Todo from './components/Todo.js';
+import Form from './components/Form.js';
+import FilterButton from './components/FilterButton.js';
 
 function App(props) {
-  const subject = props.subject;
+
+  const taskList = props.tasks?.map((task) => (
+    <Todo 
+      id={ task.id } 
+      name={ task.name } 
+      completed={ task.completed } 
+      key={ task.id }
+      />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {subject}!
-        </p>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+        >
+        {taskList}
+      </ul>
     </div>
   );
 }
